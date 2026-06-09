bcfcc4fb290aefe81239f07e174ef07e7fc2dd0a
luanrichardsz <luanrchardof@gmail.com>
2026-06-09 19:49:20 -0300

    fetch(correction): #3 - Correção da hierarquia de pastas e corrige a conexão com o banco local para testes

--- certifiquese/src/main/java/br/com/certifiquese/model/UsuarioEntity.java
+++ certifiquese/src/main/java/br/com/certifiquese/model/UsuarioEntity.java
@@ -1,6 +1,5 @@
-package br.com.certifiquese.usuario;
+package br.com.certifiquese.model;
 
-import br.com.certifiquese.certificado.CertificadoEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,7 +7,8 @@ import java.util.List;
 
 @Entity
 @Table(name = "tb_usuario")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioEntity {
@@ -20,14 +20,12 @@ public class UsuarioEntity {
     private String nomeUsuario;
     private String biografia;
 
-    @Column(nullable = false, unique =  true)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String senha;
 
-    //mappedBy para falar pro spring que o usuario é a classe "pai", sem o mappedBy criaria uma nova tabela relacional das FK
-    //Cascade define o que vamos conseguir mexer nesse objeto, o ALL Salva, Exclui e Merge (existe essas funcoes unicas no cascade)
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<CertificadoEntity> certificados;
 }
