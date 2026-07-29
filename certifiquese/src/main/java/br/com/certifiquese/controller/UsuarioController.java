df9042a2f2781670ba8a43f50fa24ba6dfe4f51b
luanrichardsz <luanrchardof@gmail.com>
2026-07-28 22:28:49 -0300

    feat(usuario) - Cria a função para retornar o proprio usuario logado

--- certifiquese/src/main/java/br/com/certifiquese/controller/UsuarioController.java
+++ certifiquese/src/main/java/br/com/certifiquese/controller/UsuarioController.java
@@ -3,10 +3,12 @@ package br.com.certifiquese.controller;
 import br.com.certifiquese.dto.UsuarioRequestDTO;
 import br.com.certifiquese.dto.UsuarioResponseDTO;
 import br.com.certifiquese.service.UsuarioService;
+import br.com.certifiquese.security.authentication.UsuarioAutenticado;
 
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,5 +30,11 @@ public class UsuarioController {
     public ResponseEntity<List<UsuarioResponseDTO>> listarTodos() {
         List<UsuarioResponseDTO> usuarios = usuarioService.listarTodos();
         return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioResponseDTO> listarUsuario(@AuthenticationPrincipal UsuarioAutenticado usuarioAutenticado) {
+        UsuarioResponseDTO usuarioLogado = usuarioService.buscarPorId(usuarioAutenticado.getId());
+        return ResponseEntity.ok(usuarioLogado);
     }
 }
